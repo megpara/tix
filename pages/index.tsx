50f98be9e3c251ca8af7f69461795f1c5a237253
Megan Paradowski <meganparadowski@Megans-MBP.lan>
2022-05-07 20:46:27 -0700

    Styling updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,25 +10,28 @@ export default function Home() {
     <motion.div
       animate={{ opacity: 1 }}
       initial={{ opacity: 0 }}
-      className="mb-10 bg-black h-full w-full text-white"
+      className="mb-10 h-full w-full text-white"
     >
-      <div className="text-red-500 underline underline-offset-8 p-8">
-        upcoming
-      </div>
-      <div className="m-5">
-        <div className="m-auto" style={{ maxWidth: 500 }}>
-          <img src="olfaction.jpeg" style={{ maxHeight: 400 }} />
-          <div className="text-gray-500 uppercase font-bold mt-5">
-            {event.dates.join(" || ")}
-          </div>
-          <Link href="/tix">
-            <div className="font-bold text-2xl mt-2 cursor-pointer hover:text-red-500">
-              {event.title}
+      <div className="">
+        <div className="text-red-500 font-bold underline underline-offset-8 text-sm m-5">
+          Upcoming
+        </div>
+
+        <div className="m-5">
+          <div className="m-auto" style={{ maxWidth: 500 }}>
+            <img src="olfaction.jpeg" style={{ maxHeight: 400 }} />
+            <div className="text-gray-500 uppercase font-bold mt-5">
+              {event.dates.join(" || ")}
             </div>
-          </Link>
-          <div className="mt-2 mb-5">
-            <img className="w-3 inline" src="location.png" />
-            <div className="inline ml-2 text-xs">{event.location}</div>
+            <Link href="/tix">
+              <div className="font-bold text-2xl mt-2 cursor-pointer hover:text-red-500">
+                {event.title}
+              </div>
+            </Link>
+            <div className="mt-2 mb-5">
+              <img className="w-3 inline" src="location.png" />
+              <div className="inline ml-2 text-xs">{event.location}</div>
+            </div>
           </div>
           <AnimatePresence>
             <motion.button
