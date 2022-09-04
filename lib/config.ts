cdc277a613252df29bb7bd27e972d5927900e1b2
aklevecz <teh@raptor.pizza>
2022-09-03 22:27:16 -0700

    config

---
+++ lib/config.ts
@@ -0,0 +1,10 @@
+export const event = {
+  title: "VOLTA X The Institute of Art and Olfaction: MILK",
+  dates: ["Thursday 11.10.2022", "Friday 11.11.2022", "Saturday 11.12.2022"],
+  location: `Institute of Art and Olfaction`,
+  time: "6:00pm",
+  address: `932 Chung King Road Los
+  Angeles CA, 90012`,
+  googleMapsLink: "https://goo.gl/maps/i4vrK9c9WLZJVVoT7",
+  slug: "MILK",
+};
