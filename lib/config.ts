3b1b535002d5ab5f2a54da6cc4b9019bc28c7e36
Megan Paradowski <meganparadowski@gmail.com>
2022-09-08 11:49:11 -0700

    Update description

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 export const event = {
   title: "VOLTA X The Institute of Art and Olfaction: MILK",
-  description: `The Institute of Art and Olfaction presents Volta's “MILK.” Drawing in part from the Greek tragedy “Medea,” the work mixes dance, poetry, sound, and scent to examine the construction of feminine desire and destruction. Audience members will be able to sample custom blends developed by the Institute of Art and Olfaction that tell "MILK's" story through scent. `,
+  description: `The Institute of Art and Olfaction presents Volta's “MILK.” Drawing in part from the Greek tragedy “Medea,” the work mixes dance, prose, sound, and scent to examine the construction of feminine desire and destruction. Writer Alexis Okeowo will orate an original epic narrative while the dancers tell the story through movement. Audience members will be able to sample custom blends developed by the Institute of Art and Olfaction that tell "MILK's" story through scent. `,
   dates: ["Thursday 11.10.2022", "Friday 11.11.2022", "Saturday 11.12.2022"],
   location: `Institute of Art and Olfaction`,
   time: "6:00pm",
