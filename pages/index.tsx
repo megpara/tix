621a7aab7d1e56dffde17de716c9a4e8c25d860b
aklevecz <teh@raptor.pizza>
2022-10-26 09:06:54 -0700

    faster loading img

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,31 +7,21 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <motion.div
-      animate={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      className="mb-10 h-full w-full text-white"
-    >
+    <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} className="mb-10 h-full w-full text-white">
       <div className="pb-10">
-        <div className="text-red-500 font-bold underline underline-offset-8 text-sm m-5">
-          Upcoming
-        </div>
+        <div className="text-red-500 font-bold underline underline-offset-8 text-sm m-5">Upcoming</div>
 
         <div className="md:m-10 m-12">
           <div className="m-auto" style={{ maxWidth: 500 }}>
-            <img src="olfactionHeaderImg.png" style={{ maxHeight: 400 }} />
-            <div className="text-gray-500 uppercase font-bold mt-5">
-              {event.dates.join(" | ")}, 6pm
-            </div>
+            <img src="olfactionHeaderImg_small.jpg" style={{ maxHeight: 400 }} />
+            <div className="text-gray-500 uppercase font-bold mt-5">{event.dates.join(" | ")}, 6pm</div>
             <Link href="/tix">
-              <div className="font-bold text-2xl mt-2 cursor-pointer hover:text-red-500">
-                {event.title}
-              </div>
+              <div className="font-bold text-2xl mt-2 cursor-pointer hover:text-red-500">{event.title}</div>
             </Link>
             <div className="mt-2 mb-5">
               <img className="w-3 inline" src="location.png" />
               <a href="https://artandolfaction.com/" rel="noreferrer" target="_blank">
-              <div className="inline ml-2 text-xs underline decoration-red-500">{event.location}</div>
+                <div className="inline ml-2 text-xs underline decoration-red-500">{event.location}</div>
               </a>
             </div>
             <div className="text-sm mb-5">{event.description}</div>
