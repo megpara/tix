c14723ccfeca1ab025f7b1e234b487bb75aeab8a
aklevecz <teh@raptor.pizza>
2022-11-08 17:21:31 -0800

    better typing

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,7 @@
-export const fulfillmentWithIntent = (fulfillments: any, intents: any) =>
-  fulfillments.map((fulfillment: any) => {
-    const intent = intents.find((intent: any) => intent.uuid === fulfillment.uuid);
+import { FulfillmentData, IntentData } from "../hooks/types";
+
+export const fulfillmentWithIntent = (fulfillments: FulfillmentData[], intents: IntentData[]) =>
+  fulfillments.map((fulfillment) => {
+    const intent = intents.find((intent) => intent.uuid === fulfillment.uuid);
     return { ...intent, ...fulfillment };
   });
